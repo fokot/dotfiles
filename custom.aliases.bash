517f90c2ba13d3c3c4aa7d20afbb6f966c1e4e29
Frantisek Kocun <frantisek.kocun@merchenta.com>
2015-12-23 01:56:27 -0500

    python server

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -8,3 +8,6 @@ alias gl='git log --graph --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(whi
 
 # better history https://github.com/hanslub42/rlwrap
 alias lein="rlwrap lein"
+
+# simple python server, default port is 8000
+alias pserver="python -m SimpleHTTPServer"
